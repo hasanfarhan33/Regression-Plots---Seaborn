5a6582a10a37a96761012e63694413bff5fa3bab
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-14 21:49:16 +0300

    Final Commit

--- regressionPlots.py
+++ regressionPlots.py
@@ -2,3 +2,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 tips = sns.load_dataset('tips')
+# sns.lmplot(x='total_bill', y='tip', data=tips, hue='sex', markers=['o', 'v'], scatter_kws={'s':100})
+# sns.lmplot(x='total_bill', y='tip', data=tips, col='smoker', row='time')
+sns.lmplot(x='total_bill', y='tip', data=tips, col='day', hue='sex', aspect=0.6, size=8)
+
+
+plt.show()
